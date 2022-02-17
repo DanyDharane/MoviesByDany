f7111b214d5a124120f8d2ac4212f2da679e8bc7
DanyD <ddharane@sg.palo-it.com>
2022-02-17 13:52:03 +0800

    Revert "Initial commit Android Project"
    This reverts commit 9bc117c47585cb08de42d530e8d919829fadabce.

--- app/src/main/java/com/example/moviesbydany/features/movies/domain/model/MovieSearchResult.kt
+++
@@ -1,7 +0,0 @@
-package com.example.moviesbydany.features.movies.domain.model
-
-data class MovieSearchResult(
-    val Response: String,
-    val Search: List<Movie>,
-    val totalResults: String
-)
--- app/src/main/java/com/example/moviesbydany/features/movies/domain/model/Rating.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moviesbydany.features.movies.domain.model
-
-data class Rating(
-    val Source: String, // Internet Movie Database
-    val Value: String // 6.8/10
-)
